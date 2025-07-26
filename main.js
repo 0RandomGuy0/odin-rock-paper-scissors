a6a782eed60039223ef32fc101b3d56192e368a7
0RandomGuy0 <linivan707@gmail.com>
2025-07-26 08:43:24 +0800

    Added logic to play a round.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-console.log(getHumanChoice());
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice(){
-    //Gets a number between 0 and 2
     let choice = Math.floor(Math.random(3) * 3);
     
     if (choice === 0) {
@@ -9,9 +9,69 @@ function getComputerChoice(){
     } else if (choice === 1) {
         return "paper";
     }
-    return "scissors";
+    return "scissor";
 }
 
+//Function assumes valid input
 function getHumanChoice(){
     return prompt("Enter rock, paper, or scissor.");
 }
+
+function playRound(humanChoice, computerChoice){
+    const hChoice = humanChoice.toLowerCase();
+    let outcome = "";
+
+    switch(hChoice){
+        case "rock":
+            if (computerChoice === "rock"){
+                outcome = "draw";
+            }
+            else if (computerChoice === "paper"){
+                outcome = "lose";
+            }
+            else{
+                outcome = "win";
+            }
+            break;
+        case "paper":
+            if (computerChoice === "rock"){
+                outcome = "win";
+            }
+            else if (computerChoice === "paper"){
+                outcome = "draw";
+            }
+            else{
+                outcome = "lose";
+            }
+            break;
+        case "scissor":
+            if (computerChoice === "rock"){
+                outcome = "lose";
+            }
+            else if (computerChoice === "paper"){
+                outcome = "win";
+            }
+            else{
+                outcome = "draw";
+            }
+            break;
+        default:
+            break;
+    }
+    
+    if (outcome === "win"){
+        humanScore++;
+    }
+    else if(outcome === "lose"){
+        computerScore++;
+    }
+
+    console.log("You chose " + hChoice + ".");
+    console.log("Computer chose " + computerChoice + ".");
+    console.log("You " + outcome + "!");
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection,computerSelection);
